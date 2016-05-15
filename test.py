5a11debfd9c8126c67f7ec083f1e7ee317f6ee58
lucarin91 <lucarin91@gmail.com>
2016-05-16 00:03:46 +0200

    cose

---
+++ test.py
@@ -0,0 +1,17 @@
+importa random
+
+per i in range(0, 10):
+    numero = random.randrange(0, 10)
+    print(numero)
+    if True:
+        salta
+    sennò False:
+        salta
+altrimenti:
+    print ('END')
+
+finchè True:
+    print ('while')
+    interrompi
+altrimenti:
+    print ('END')
